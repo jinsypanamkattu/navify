bb386f2f5b56d94fa9a2393120bf95e05db78733
Jinsy <jinsypanamkattu@gmail.com>
2025-01-09 07:42:24 +0530

    Updated for including createbrowserrouter

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,32 @@ import { createRoot } from 'react-dom/client'
 //import './index.css'
 import App from './App.jsx'
 
+
+
+import { Home } from './pages/Home';
+import { About } from './pages/About';
+import UserListing from './pages/UserListing';
+import UserDetail from './pages/UserDetail';
+import NotFound from './pages/NotFound';
+
+
+import {createBrowserRouter,RouterProvider} from 'react-router-dom'
+const router=createBrowserRouter([
+  {
+    path:'/',
+    element:<App/>,
+    children:[
+      {path:"/",element:<Home/>},
+      {path:"/about",element:<About/>},
+      {path:"/users", element:<UserListing />},
+      {path:"/user/:id", element:<UserDetail />},
+      {path:"*", element:NotFound }
+    ]
+  }
+])
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
